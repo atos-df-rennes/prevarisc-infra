4e9bd95f2465bd9827f13af6b30132cb10781974
Maxime Merrien <maxime.merrien@atos.net>
2025-09-02 09:02:22 +0200

    Commande d'installation de Prevarisc

--- .castor/castor.php
+++ .castor/castor.php
@@ -2,14 +2,9 @@
 
 use Castor\Attribute\AsTask;
 
-use Symfony\Component\Process\ExecutableFinder;
-use function Castor\check;
-use function Castor\fs;
 use function Castor\import;
 use function Castor\io;
-use function Castor\notify;
 use function Castor\parallel;
-use function Castor\run;
 
 import(__DIR__);
 
@@ -71,45 +66,4 @@ function test(): void
             symfonyTest();
         }
     );
-}
-
-#[AsTask(namespace: 'prevarisc', description: 'Setup Prevarisc')]
-function setup(): void
-{
-    io()->title('Setting up Prevarisc.');
-
-    check(
-      'Checking if Git is installed.',
-      'Git is not installed. Please install it first.',
-      fn () => (new ExecutableFinder())->find('git')
-    );
-
-    io()->section('Cloning repositories.');
-
-    io()->text('Cloning Prevarisc repository.');
-    run(['git', 'clone', 'https://github.com/atos-df-rennes/prevarisc.git']);
-
-    io()->info('You will need a Personal Access Token for the next operation. Please create one if you do not have one already.');
-
-    $resumeScript = io()->confirm('Resume script?');
-    if (!$resumeScript) {
-        return;
-    }
-
-    io()->text('Cloning Prevarisc Migration repository.');
-    run(['git', 'clone', 'https://github.com/atos-df-rennes/prevarisc-migration.git']);
-
-    io()->text('Cloning Prevarisc Passerelle Plat\'AU repository.');
-    run(['git', 'clone', 'https://github.com/atos-df-rennes/prevarisc-passerelle-platau.git']);
-
-    io()->section('Copying web server files.');
-
-    io()->text('Copying configuration file.');
-    fs()->copy('apache/httpd-prevarisc-config.conf.example', 'apache/httpd-prevarisc-config.conf');
-
-    io()->text('Copying version file.');
-    fs()->copy('apache/httpd-prevarisc-version.conf.example', 'apache/httpd-prevarisc-version.conf');
-
-    // @todo: Voir pour faire fonctionner la notification
-    // notify('Prevarisc is ready!');
 }
