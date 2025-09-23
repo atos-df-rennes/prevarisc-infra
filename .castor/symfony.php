d4cfec1ea1ea6275c08289d4468083a2d302271d
Antoine Bourget <antoine.bourget.external@atos.net>
2025-09-23 15:41:06 +0200

    chore(task): ajouter une tâche pour exécuter des commandes console

--- .castor/symfony.php
+++ .castor/symfony.php
@@ -1,8 +1,22 @@
 <?php
 
 use Castor\Attribute\AsTask;
+use Castor\Attribute\AsRawTokens;
 use function Castor\io;
 use function Castor\run;
+
+#[AsTask(name: 'console', namespace: 'symfony', description: 'Execute Symfony console command')]
+function symfonyConsole(#[AsRawTokens] array $args): void
+{
+    io()->title('Executing Symfony console command');
+
+    run(
+        [
+            'docker', 'compose', '--file', 'compose.dev.yaml', 'exec', '-w', '/var/www/html/prevarisc-migration',
+            'app', 'php', 'bin/console', ...$args
+        ]
+    );
+}
 
 #[AsTask(name: 'cs-check', namespace: 'symfony', description: 'Check coding style for Symfony application')]
 function symfonyCsCheck(): void
@@ -52,5 +66,5 @@ function symfonyMigrate(): void
     io()->title('Migrating database schema');
 
     io()->section('Executing Doctrine migrations');
-    run(['docker', 'compose', '--file', 'compose.dev.yaml', 'exec', '-w', '/var/www/html/prevarisc-migration', 'app', 'php', 'bin/console', 'd:m:m', '--no-interaction']);
+    symfonyConsole(['d:m:m', '--no-interaction']);
 }
